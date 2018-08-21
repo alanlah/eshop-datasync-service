6ee63f4fc354f69775f574dbb0cc227dc2e0c467
alan123 <892881202@qq.com>
2018-08-21 15:01:26 +0800

    first commit

---
+++ src/main/java/com/eshop/eureka/service/EshopProductServiceFallback.java
@@ -0,0 +1,43 @@
+package com.eshop.eureka.service;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class EshopProductServiceFallback implements EshopProductService {
+
+	public String findBrandById(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String findBrandByIds(String ids) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String findCategoryById(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String findProductIntroById(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String findProductPropertyById(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String findProductById(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String findProductSpecificationById(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
